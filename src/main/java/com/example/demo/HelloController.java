9565a205aecd39e24f42ad193911107eae091b3d
alina-yur <alina.yurenko@oracle.com>
2023-11-01 11:10:42 +0100

    first commit

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello from GraalVM and Spring!💃";
+    }
+    
+}
