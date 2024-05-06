bb12059a9088d16773647f168d6624a697899ece
alina-yur <alina.yurenko@oracle.com>
2024-05-06 12:08:44 +0200

    add resource hints

--- src/main/java/com/example/demo/ResourceController.java
+++ src/main/java/com/example/demo/ResourceController.java
@@ -1,15 +1,21 @@
 package com.example.demo;
 
+import org.springframework.aot.hint.RuntimeHints;
+import org.springframework.aot.hint.RuntimeHintsRegistrar;
+import org.springframework.context.annotation.ImportRuntimeHints;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.util.FileCopyUtils;
 
+import com.example.demo.ResourceController.ResourceControllerHints;
+
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 @RestController
+@ImportRuntimeHints(ResourceControllerHints.class)
 public class ResourceController {
 
     @GetMapping("/resource")
@@ -19,6 +25,14 @@ public class ResourceController {
             return FileCopyUtils.copyToString(reader);
         } catch (IOException e) {
             return "Failed to load resource";
+        }
+    }
+
+    static class ResourceControllerHints implements RuntimeHintsRegistrar {
+
+        @Override
+        public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
+            hints.resources().registerResource(new ClassPathResource("message.xml"));
         }
     }
 }
