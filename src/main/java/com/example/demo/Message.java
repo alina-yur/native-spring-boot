3f1b55b073c4a8411c773efcef6bf92a28ec7cae
Stéphane Nicoll <stephane.nicoll@broadcom.com>
2024-07-08 13:54:52 +0200

    Use ReflectiveScan
    This commit uses a new feature in Spring Framework 6.2 that allows to
    scan arbitrary types for reflective information. Previously, only Spring
    beans were considered.

--- src/main/java/com/example/demo/Message.java
+++ src/main/java/com/example/demo/Message.java
@@ -1,6 +1,9 @@
 package com.example.demo;
 
+import org.springframework.aot.hint.annotation.Reflective;
+
 public class Message {
-    
+
+	@Reflective
     public static final String MESSAGE = "Hello from Spring Native!🤖";
 }
