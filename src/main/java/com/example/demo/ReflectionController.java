ff4dfd5b91d4d794263db44422e06b2c779f5d75
alina-yur <alina.yurenko@oracle.com>
2024-09-13 13:52:38 +0200

    revert to hints on the caller site

--- src/main/java/com/example/demo/ReflectionController.java
+++ src/main/java/com/example/demo/ReflectionController.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.springframework.aot.hint.annotation.RegisterReflectionForBinding;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.Arrays;
@@ -13,6 +14,7 @@ public class ReflectionController {
         return getMessage();
     }
 
+    @RegisterReflectionForBinding(Message.class)
     private String getMessage() {
         try {
             String className = Arrays.asList("com", "example", "demo", "Message").stream()
