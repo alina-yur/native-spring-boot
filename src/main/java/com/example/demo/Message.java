f6934f109f3b741a1d6058325578592ded46c971
alina-yur <alina.yurenko@gmail.com>
2024-09-30 13:11:39 +0000

    remove the import as well 🤦‍♀️

--- src/main/java/com/example/demo/Message.java
+++ src/main/java/com/example/demo/Message.java
@@ -1,7 +1,5 @@
 package com.example.demo;
 
-import org.springframework.aot.hint.annotation.Reflective;
-
 public class Message {
 
     public static final String MESSAGE = "Hello from Spring Native!🤖";
